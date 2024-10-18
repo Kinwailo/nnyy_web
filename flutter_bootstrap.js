131e8783b593fcff582889784cbe3808a55bc9d5
Kinwailo <lokinwai@gmail.com>
2024-10-18 18:15:24 +0800

    up

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2399896567"
+    serviceWorkerVersion: "3548418256"
   }
 });
